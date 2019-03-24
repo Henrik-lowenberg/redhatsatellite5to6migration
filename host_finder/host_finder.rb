df9b40290f6443d47cfc7e48d785733f41d8a367
Antony Sastre <antony@thenewconcept.com>
2019-03-24 13:13:24 +0100

    Refactored and fixed host finder runner.

---
+++ host_finder/host_finder.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+# Created 2019-03-15
+# # By: Henrik Lowenberg
+# # Property of HCL Inc.
+# # Description: Migrate hosts from Satellite 5 to Satellite 6 in a series of steps
+
+class HostMatcher
+  attr_reader :listFile
+
+  def initialize(listFile)
+    @listFile = listFile
+    @search   = Array.new
+    @found    = Array.new
+    @results  = Array.new
+  end
+
+  def run!
+    file_to_array
+    process_array
+    print_results
+  end
+
+  private
+
+  def file_to_array
+    File.open(@listFile, 'r').each { |line| @search << line }
+    @search.map! { |search| search.split('.')[0].strip }
+  end
+
+  def process_array
+    File.open("hosts").each_line do |line|
+      @search.each do |search|
+        if line.match(/#{search}/)
+          @results << line
+          @found   << search
+        end
+      end
+    end
+  end
+
+  def print_results
+    if @results.any?
+      puts "\n======= MATCHING:\n"
+      puts @results
+
+      puts "\n======= MISSING:\n"
+      puts @search - @found
+    else
+      puts "No FQDN names could be found in global hosts file."
+      puts "Please update global hosts file on jumphost with: `ip hostname FQDN`"
+    end
+  end
+end
+
+unless ARGV.empty?
+  if ARGV.first.start_with?("-")
+    case ARGV.shift
+    when '-f', '--file'
+      listFile = ARGV[0]
+      HostMatcher.new(listFile).run!
+    when '-h', '--help'
+      puts "Usage: #{__FILE__} -f filename"
+      puts "filename should be a text file comprized of hosts you want to migrate from Satellite 5 to Satellite 6"
+    when '-v', '--version'
+      puts "HostMatcher 1.0.0"
+    end
+  end
+else
+  puts
+  puts "Usage:"
+  puts " #{__FILE__} -f|--file <filename>"
+  puts " #{__FILE__} -h|--help"
+  puts " #{__FILE__} --version"
+end

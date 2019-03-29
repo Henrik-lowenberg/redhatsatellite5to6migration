c8fb65a17c0beee99aef9d433b1404990d293dfd
henrik.lowenberg <henrik.lowenberg@volvo.com>
2019-03-29 15:35:09 +0100

    added dump files and enc

--- sat526Migration/sat526MigrationGetHost_Enc.rb
+++ sat526Migration/sat526MigrationGetHost_Enc.rb
@@ -7,5 +7,35 @@
 #
 # # This file: sat526MigrationGetHost_Enc.rb
 
+=begin
+Format of binary file data:
+
+1dyfqn2:
+  parameters:
+    base: ubuntu01
+    customer: volvo
+    devicetype: ws
+    site: got
+    supportlevel: basic
+    sz: vcn
+    uuma: got
+
+=end
+
 puts " #{__FILE__} running..."
 puts "Halleluja Lord! I can see the light!" if $hosts_fqdn.any?
+puts
+
+=begin
+File.open('/data01/puppet/enc/combined.dump', "r") do |f|
+  @dumpfile = Marshal.load(f)
+end
+=end
+
+encdump = []
+File.open('/data01/puppet/enc/combined.yaml', "r") do |file|
+  file.each_line do |line|
+    encdump << line
+  end
+end
+p encdump.inspect
